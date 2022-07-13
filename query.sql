53c5e7ee24825258c2d7228286b296b4a42648fa
Khushi-pal <103887874+Khushi-pal@users.noreply.github.com>
2022-07-13 18:17:00 +0530

    Add files via upload

---
+++ query.sql
@@ -0,0 +1,27 @@
+create table STUDENTS (
+ id int auto_increment,
+ name varchar(50),
+ usn varchar(10),
+ year int,
+ college varchar(50),
+ primary key(id)
+);
+/* Insert values to the table */
+insert into students (name, usn, year, college) values (
+ 'kittu', '4SF18IS026', 2017, 'AJIET'
+);
+insert into students (name, usn, year, college) values (
+ 'Swathi', '4SF19IS071', 2018, 'AJIET'
+);
+insert into students (name, usn, year, college) values (
+ 'Prathiksha', '4SF18IS069', 2019, 'AJIET'
+);
+insert into students (name, usn, year, college) values (
+ 'YASHI', '4SF18IS111', 2018, 'AJIET'
+);
+insert into students (name, usn, year, college) values (
+ 'ABHI', '4SF19IS677', 2018, 'AJIET'
+);
+insert into students (name, usn, year, college) values (
+ 'YUKI', '4SF18IS031', 2017, 'AJIET'
+);
